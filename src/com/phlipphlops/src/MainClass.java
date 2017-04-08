cf47b4db87fb5f56703a0d52661ddda4b67bcded
PhlipPhlops <phillkin@gmail.com>
2017-04-08 18:23:58 -0400

    Window class

--- src/com/phlipphlops/src/MainClass.java
+++ src/com/phlipphlops/src/MainClass.java
@@ -1,9 +1,19 @@
 package com.phlipphlops.src;
 
-public class MainClass {
+import javax.swing.*;
 
+public class MainClass {
+// Creates a window of specified size, centers it, and makes sure it exits on close
     public static void main(String[] args){
-        
+
+        JFrame frame = new JFrame();
+        frame.pack();
+        frame.setSize(640,480);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
 
 }
